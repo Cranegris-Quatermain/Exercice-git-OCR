48f4501bcf84b5f140af2c0805d0ce15a2e0b05c
CraneGris-Quatermain <jeanfrancoisjonathan@gmail.com>
2015-03-06 15:40:30 +0100

    le fichier connexion.php à été modifier du procédural au mode objet. il y a donc une class database de créer plus deux function de connexion et d'execution de requette

--- connexion.php
+++ connexion.php
@@ -1,3 +1,23 @@
 <?php
-$bd = new PDO('mysql:host=localhost;dbname=test;charset=utf8','root','ryuk', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+abstract class DATABASE{
+	private $db;
+
+	protected function exQuery($requet, $param = null){
+		if($param == null){
+			$resultat = $this->DB()->query($requet);
+		}else{
+			$resultat= $this->DB()->prepare($requet);
+			$resultat->execute($param);
+		}
+		return $resultat;
+	}
+
+	private function DB(){
+		$PDO_EXCEP=array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
+		if($this->db == null){
+			$this->bd = new PDO('mysql:host='.SRV.';dbname='.DB.';charset=utf8',USR,PWD, array($PDO_EXCEP);
+		}
+		return $this->db;
+	}
+}	
 ?>
